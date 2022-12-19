4ccb827f5b2c674660f706468a1ca7820d48dd70
Penelope Chen <pen.c828@gmail.com>
2022-12-19 17:09:43 +1100

    added js comments & plans

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // testing if console actually works
 console.log("yeehaw")
 
+// geeksforgeeks.org/simple-tic-tac-toe-game-using-javascript/ makes the most sense but also isn't that DRY
+
 // declare TTT squares as variables & their set DOM
 const b0 = document.getElementById("top-left")
 const b1 = document.getElementById("top-mid")
@@ -11,6 +13,8 @@ const b5 = document.getElementById("mid-right")
 const b6 = document.getElementById("bottom-left")
 const b7 = document.getElementById("bottom-mid")
 const b8 = document.getElementById("bottom-right")
+
+// need to input x's & o's using .addEventListener, onclick & inner.HTML/text.Content
 
 // declaring match 3's as arrays (might need to fix this)
 let topRow = [b0, b1, b2]
