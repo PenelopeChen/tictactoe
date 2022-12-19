e9953a53825b385f603abce78a02ecf305cfaa2f
Penelope Chen <pen.c828@gmail.com>
2022-12-19 16:35:34 +1100

    js dom & some arrays

---
+++ script.js
@@ -0,0 +1,23 @@
+// testing if console actually works
+console.log("yeehaw")
+
+// declare TTT squares as variables & their set DOM
+const b0 = document.getElementById("top-left")
+const b1 = document.getElementById("top-mid")
+const b2 = document.getElementById("top-right")
+const b3 = document.getElementById("mid-left")
+const b4 = document.getElementById("mid-mid")
+const b5 = document.getElementById("mid-right")
+const b6 = document.getElementById("bottom-left")
+const b7 = document.getElementById("bottom-mid")
+const b8 = document.getElementById("bottom-right")
+
+// declaring match 3's as arrays (might need to fix this)
+let topRow = [b0, b1, b2]
+let midRow = [b0, b1, b2]
+let bottomRow = [b0, b1, b2]
+let leftColumn = [b0, b1, b2]
+let midColumn = [b0, b1, b2]
+let rightColumn = [b0, b1, b2]
+let diagonal1 = [b0, b1, b2]
+let diagonal2 = [b0, b1, b2]
